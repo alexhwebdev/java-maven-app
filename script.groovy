c70b7f289737162580ea9ac66e76f21eb721a243
Young Hong <a.hong@hidonix.com>
2026-06-22 15:17:01 -0700

    update *

--- script.groovy
+++ script.groovy
@@ -2,21 +2,21 @@ def buildApp() {
     echo 'building the application...'
 }
 
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
-}
+// def buildJar() {
+//     echo 'building the application...'
+//     sh 'mvn package'
+// }
 
-def buildImage() {
-    echo 'building the docker image...'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        // sh "docker build -t nanatwn/demo-app:jma-2.0 ."
-        sh "docker build -t alexhwebdev/nana-demo-app:jma-2.0 ."
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        // sh "docker push nanatwn/demo-app:jma-2.0"
-        sh "docker push alexhwebdev/nana-demo-app:jma-2.0"
-    }
-}
+// def buildImage() {
+//     echo 'building the docker image...'
+//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+//         // sh "docker build -t nanatwn/demo-app:jma-2.0 ."
+//         sh "docker build -t alexhwebdev/nana-demo-app:jma-2.0 ."
+//         sh 'echo $PASS | docker login -u $USER --password-stdin'
+//         // sh "docker push nanatwn/demo-app:jma-2.0"
+//         sh "docker push alexhwebdev/nana-demo-app:jma-2.0"
+//     }
+// }
 
 def deployApp() {
     echo 'deploying the application...'
